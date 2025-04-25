2324b3b2aa04efa99d0fc01ccef27d5020a50280
Bastien ADAM <bastien.adam.buom01@gmail.com>
2025-04-25 20:41:16 +0200

    Update main

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include "libft_malloc.h"
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	show_alloc_mem();
 	printf("Allocating %lu bytes for `%s`\n", strlen(argv[1]) + 1,  argv[1]);	
 
-	void *ptr = ft_malloc(strlen(argv[1]) + 1);
+	void *ptr = malloc(strlen(argv[1]) + 1);
 
-	show_alloc_mem();	
 
 	printf("Allocated at %p\n", ptr);
 
@@ -19,4 +19,23 @@ int main(int argc, char **argv)
 
 	printf("Copyed as: \"%s\"\n", (char *)ptr);
 
+
+	show_alloc_mem();	
+
+	ptr = realloc(ptr, strlen(argv[2]) + 1);
+
+
+	printf("Allocated realloc at %p\n", ptr);
+
+	if (!ptr)
+		return 1;
+
+	memcpy(ptr, argv[2], strlen(argv[2]) + 1);
+
+	printf("Copyed realloc as: \"%s\"\n", (char *)ptr);
+
+
+	show_alloc_mem();
+
+	free(ptr);
 }
