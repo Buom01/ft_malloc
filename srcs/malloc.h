2636206c1ee8ae0d5e23dd735ff9033fc455cac5
Bastien ADAM <badam@f2r10s7.clusters.42paris.fr>
2025-10-26 16:34:20 +0100

    libft_malloc.h -> malloc.h

--- srcs/malloc.h
+++ srcs/malloc.h
@@ -1,8 +1,24 @@
 #ifndef MALLOC_H
 # define MALLOC_H
 
-// Redirect to our malloc, used for the external tester that use `malloc.h` instead of `libft_malloc.h`
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/mman.h>
+# include <stdbool.h>
+# include <sys/resource.h>
 
-# include "libft_malloc.h"
+void free(void *ptr);
+void *malloc(size_t size);
+void *calloc(size_t n, size_t size);
+void *realloc(void *ptr, size_t size);
+
+void show_alloc_mem();
+void show_alloc_mem_ex();
+
+char *ft_ptrtoa_noalloc(void *ptr);
+void ft_putptr_fd_noalloc(void *ptr, int fd);
+char *ft_itoa_noalloc(int n);
+void ft_putnbr_fd_noalloc(int n, int fd);
+
 
 #endif
