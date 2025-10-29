197754079c0ac2296324a36883d2d572dbfd580b
Bastien ADAM <bastien.adam.buom01@gmail.com>
2025-10-29 01:39:05 +0100

    Add comments about allocation and alignement

--- srcs/allocations.h
+++ srcs/allocations.h
@@ -4,7 +4,7 @@
 # include "malloc.h"
 
 # define ALLOCS_COUNT 128 // Number of allocation for a single zone
-# define TINY_ALLOC_SIZE 128 // n
+# define TINY_ALLOC_SIZE 128 // n — Allocation up to SIZE can go in that zone
 # define SMALL_ALLOC_SIZE 2048 // m
 # define TINY_ZONE_SIZE (sysconf(_SC_PAGESIZE)) * 10 // N — Representing a single zone size
 # define SMALL_ZONE_SIZE (sysconf(_SC_PAGESIZE) * 500) // M
@@ -12,8 +12,8 @@
 # define SMALL_ZONES_COUNT 1024
 # define BIG_ZONES_COUNT 1024 * ALLOCS_COUNT
 
-# define ALIGNEMENT 16
-# define ALIGN(size) (((size) + (ALIGNEMENT - 1)) & ~(ALIGNEMENT - 1));
+# define ALIGNEMENT 16  // Bytes
+# define ALIGN(size) (((size) + (ALIGNEMENT - 1)) & ~(ALIGNEMENT - 1));  // Round up at ALIGNEMENT on the ceil — Popular formula
 
 typedef struct s_alloc
 {
