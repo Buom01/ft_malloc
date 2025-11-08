fab209cab81fd23904ed8823c28c77dd914bc6f0
Bastien ADAM <badam@f2r9s5.clusters.42paris.fr>
2025-11-08 15:03:08 +0100

    Mutex: Use simple static initializer and thus use only one global variable for it

--- srcs/multithreading.c
+++ srcs/multithreading.c
@@ -1,17 +1,10 @@
 #include "malloc.h"
 #include "multithreading.h"
 
-static pthread_once_t once_control = PTHREAD_ONCE_INIT;
 pthread_mutex_t malloc_mutex_g = PTHREAD_MUTEX_INITIALIZER;
-
-void initialize_mutex()
-{
-    pthread_mutex_init(&malloc_mutex_g, NULL);
-}
 
 void malloc_lock_mutex()
 {
-    pthread_once(&once_control, initialize_mutex);
     pthread_mutex_lock(&malloc_mutex_g);
 }
 
